83a0303933fe5d98cf1621c279b43c38834ef3a3
Icarz <mr.rghay@email.com>
2025-06-10 19:15:45 +0100

    starting

---
+++ src/Components/Todo.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+const Todo = () => {
+  const [todo, setTodo] = useState(""); // this is only for the input values to have the dynamic//
+  const [todos, setTodos] = useState([]); // this is for the array of data that must me in the array //
+
+  function handleSubmit(e) {
+    e.preventDefault(); // this to prevent the refreshing for the page when we click on the button//
+    setTodos([...todos, todo]); // the spread operator keeps the pervious values in the array//
+    setTodo("");
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={todo}
+          type="text"
+          placeholder="your tasks here ..."
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
+      {console.log(todos)}
+    </div>
+  );
+};
+
+export default Todo;
