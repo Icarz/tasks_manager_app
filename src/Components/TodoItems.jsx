f8cfa8515bd2e22c3ee5af2014d3e7f46abdc106
Icarz <mr.rghay@email.com>
2025-06-15 16:12:11 +0100

    ongoing

--- src/Components/TodoItems.jsx
+++ src/Components/TodoItems.jsx
@@ -2,7 +2,14 @@ import styles from "./todoitems.module.css";
 const TodoItems = ({ item }) => {
   return (
     <div className={styles.items}>
-      <div className={styles.itemName}>{item}</div>
+      <div className={styles.itemsContainer}>
+        <div className={styles.itemName}>
+          {item}
+          <span>
+            <button className={styles.deleteButton}>x</button>
+          </span>
+        </div>
+      </div>
       <hr className={styles.line} />
     </div>
   );
